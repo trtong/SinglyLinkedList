ee0b428378894f351e40f3232bf812c8322f93a5
Trinh Tong <trinhtong@Zipcoders-MBP-8.hsd1.pa.comcast.net>
2018-11-02 10:10:34 -0400

    created structure and added methods -> add, remove, get, size

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -4,4 +4,26 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList<Integer> s = new SinglyLinkedList<Integer>();
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        s.add(40);
+
+        s.remove(2);
+
+        System.out.println(s.size());
+
+        for (int i = 0; i < s.size(); i++) {
+            System.out.println(s.get(i));
+        }
+    }
+
+    // new linked list (copy) = deep copy || link up data, not address
+    // create new linked list
+    // iterate through original grabbing data, new nodes in the new list, adding data in order
+    // maybe easier to do it reverse order
 }
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,121 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private int size;
+    private class Node<T> {
+
+        private T data;
+        private Node<T> address;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node<T> address) {
+            this.data = data;
+            this.address = address;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getAddress() {
+            return address;
+        }
+
+        public void setAddress(Node<T> address) {
+            this.address = address;
+        }
+    }
+
+    // singly linkedlist of T, only including nodes of T
+
+    private Node<T> head;
+
+    public SinglyLinkedList(Node<T> head) {
+        this.head = head;
+    }
+
+    public SinglyLinkedList() {
+    }
+
+    public void add(T data){
+        size++;
+        if (head == null) {
+            head = new Node<T>(data);
+        } else {
+            Node<T> tail = head;
+
+            while (tail.getAddress() != null) {
+                tail = tail.getAddress();
+            }
+
+            tail.setAddress(new Node<T>(data));
+        }
+    }
+
+    public void remove(int index) {
+        // for loop when i = index
+        // remove that node
+        // link the prior nodes address to the node after the removed one
+        if (index < 0 || index >= size) {
+            throw new RuntimeException("Node does not exist within bounds.");
+        } else {
+            Node<T> cur = head;
+            Node<T> prior = null;
+
+            for (int i = 0; i < index; i++) {
+                prior = cur;
+                cur = cur.getAddress();
+            }
+
+            if (prior == null) {
+                head = cur.getAddress();
+            } else {
+                prior.setAddress(cur.getAddress());
+            }
+            size--;
+        }
+
+    }
+
+    public boolean contains() {
+        return false;
+    }
+
+    public int find() {
+        return 0;
+    }
+
+    public Integer size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new RuntimeException("Node does not exist within bounds.");
+        } else {
+            Node<T> cur = head;
+            for (int i = 0; i < index; i++) {
+                cur = cur.getAddress();
+            }
+
+            return cur.getData();
+        }
+    }
+
+    public SinglyLinkedList<T> copy(SinglyLinkedList<T> list) {
+        return null;
+    }
+
+    public SinglyLinkedList<T> sort(SinglyLinkedList<T> list) {
+        return null;
+    }
+
 }
